44153d9bc021ac133686ce33c2661e637fd3a7d5
The Jumper <maximilian.springer@web.de>
2011-12-15 20:22:10 +0100

    New Functions: whoami, gettime, help, setnick -only for server

--- Chat-Server/Module1.vb
+++ Chat-Server/Module1.vb
@@ -1,13 +1,18 @@
 ﻿Imports System.Net
 Imports System.Net.Sockets
 Imports System.Text
+Imports System.IO
+Imports System
 Module Module1
     Dim ip As IPAddress = IPAddress.Parse("127.0.0.1")
     Dim port As Integer = 27590
     Dim server As TcpListener = Nothing
     Dim bytes(1024) As Byte
     Dim nachricht As String = Nothing
-    Dim Adressen(40) As IPAddress
+    Dim Adressen(40) As String
+    Dim Nicks(40) As String
+    Dim AdressenZaehler As Integer = 0
+    Dim RohAdressen() As String
     Sub Main()
         Console.Title = "OpenSchoolChat - Server"
         Console.ForegroundColor = ConsoleColor.White
@@ -32,9 +37,13 @@ Module Module1
         Console.ForegroundColor = ConsoleColor.White
         Console.WriteLine("Listening for connections...")
         While True
-            'assynchron()
-
             Dim client As TcpClient = server.AcceptTcpClient
+            Dim ep As Net.IPEndPoint = CType(client.Client.RemoteEndPoint, Net.IPEndPoint)
+            Dim ad As Integer = 0
+            RohAdressen = Split(ep.ToString, ":", 2)
+            Adressen(AdressenZaehler) = RohAdressen(0)
+            AdressenZaehler = AdressenZaehler + 1
+            Nicks(AdressenZaehler) = ""
             Console.ForegroundColor = ConsoleColor.Green
             Console.WriteLine("Client {0} verbunden." & vbCrLf, client.Client.RemoteEndPoint)
             While client.Connected
@@ -44,20 +53,42 @@ Module Module1
                     i = stream.Read(bytes, 0, bytes.Length)
                     nachricht = System.Text.Encoding.ASCII.GetString(bytes, 0, i)
                     Console.ForegroundColor = ConsoleColor.Cyan
-                    Console.WriteLine("{0} um " & TimeOfDay & " Uhr", client.Client.RemoteEndPoint)
+                    If Nicks(AdressenZaehler) = "" Then
+                        Console.WriteLine("{0} um " & TimeOfDay & " Uhr", client.Client.RemoteEndPoint)
+                    Else
+                        Console.WriteLine("{0} um " & TimeOfDay & " Uhr", Nicks(AdressenZaehler))
+                    End If
                     Console.ForegroundColor = ConsoleColor.White
                     Console.WriteLine(nachricht)
-                    If nachricht = "/help" Then
-                        Dim functions(1024) As Byte
-                        functions = System.Text.Encoding.ASCII.GetBytes("Hier erscheinen die spaeteren Befehle")
-                        client.GetStream.Write(functions, 0, functions.Length)
-                    ElseIf nachricht = "/file" Then
-                        Dim functions(1024) As Byte
-                        functions = System.Text.Encoding.ASCII.GetBytes("Spaeter Funktion zur Datenuebertragung")
-                        client.GetStream.Write(functions, 0, functions.Length)
-                    Else
-                        client.GetStream.Write(bytes, 0, i)
+                    If nachricht.Substring(0, 1) = "/" Then
+                        If nachricht.Substring(1, 4) = "help" Then
+                            Dim functions(1024) As Byte
+                            functions = System.Text.Encoding.ASCII.GetBytes("/whoami - Your IP")
+                            client.GetStream.Write(functions, 0, functions.Length)
+                            functions = System.Text.Encoding.ASCII.GetBytes("/gettime - Get date and time")
+                            client.GetStream.Write(functions, 0, functions.Length)
+                        ElseIf nachricht.Substring(1, 4) = "file" Then
+                            Dim functions(1024) As Byte
+                            functions = System.Text.Encoding.ASCII.GetBytes("Spaeter Funktion zur Datenuebertragung")
+                            client.GetStream.Write(functions, 0, functions.Length)
+                        ElseIf nachricht.Substring(1, 6) = "whoami" Then
+                            Dim functions(1024) As Byte
+                            functions = System.Text.Encoding.ASCII.GetBytes("whoami - You are " + Adressen(AdressenZaehler - 1))
+                            client.GetStream.Write(functions, 0, functions.Length)
+                        ElseIf nachricht.Substring(1, 7) = "gettime" Then
+                            Dim functions(1024) As Byte
+                            functions = System.Text.Encoding.ASCII.GetBytes("gettime - Today it's the " + Date.Now)
+                            client.GetStream.Write(functions, 0, functions.Length)
+                        ElseIf nachricht.Substring(1, 7) = "setnick" Then
+                            Nicks(AdressenZaehler) = nachricht.Substring(9, nachricht.Length - 9)
+                            Dim functions(1024) As Byte
+                            functions = System.Text.Encoding.ASCII.GetBytes("setnick - Your nickname is now " + nachricht.Substring(9, nachricht.Length - 9))
+                            client.GetStream.Write(functions, 0, functions.Length)
+                        Else
+                            client.GetStream.Write(bytes, 0, i)
+                        End If
                     End If
+
                 Catch e As Exception
                     Console.ForegroundColor = ConsoleColor.Red
                     Console.WriteLine(vbCrLf & "Connection closed!")
@@ -66,30 +97,5 @@ Module Module1
             End While
         End While
     End Sub
-    Sub assynchron()
-	'ein sinnloser Kommentar mit mehr Wörtern
-        Dim ips As Integer = 0
-        Dim f As Integer
-        Dim g As Integer
-        Dim h As Integer
-        Dim j As Integer
-        For f = 0 To 255
-            For g = 0 To 255
-                For h = 0 To 255
-                    For j = 0 To 255
-                        Try
-                            Dim thisip As IPAddress = IPAddress.Parse(f + "." + g + "." + h + "." + j)
-                            Dim thisserver = New TcpListener(thisip, port)
-                            thisserver.Start()
-                            Dim thisclient As TcpClient = thisserver.AcceptTcpClient()
-                            Adressen(ips) = thisip
-                            ips = ips + 1
-                        Catch e As Exception
-                        End Try
 
-                    Next
-                Next
-            Next
-        Next
-    End Sub
 End Module
