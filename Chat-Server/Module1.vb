e87786e5b832e1373b1adce9962139d2a4d5a75b
MightAndMagic <dada.dudu1@web.de>
2011-12-19 18:44:41 +0100

    Multithreading improvements.

--- Chat-Server/Module1.vb
+++ Chat-Server/Module1.vb
@@ -9,7 +9,8 @@ Module Module1
     Dim server As TcpListener = Nothing
     Dim bytes(1024) As Byte
     Dim nachricht As String = Nothing
-    Dim Adressen(40) As String
+    Dim ipAdressen(40) As IPAddress
+    Dim Adressen(40) As IPEndPoint
     Dim Nicks(40) As String
     Dim AdressenZaehler As Integer = 0
     Dim RohAdressen() As String
@@ -27,79 +28,76 @@ Module Module1
             server.Start()
             Console.ForegroundColor = ConsoleColor.Green
             Console.WriteLine("Server created on port {0}.", port)
+            Console.ForegroundColor = ConsoleColor.White
+            Console.WriteLine("Listening for connections...")
         Catch e As Exception
             Console.ForegroundColor = ConsoleColor.Red
             Console.WriteLine("Socket exception: {0}", e)
         End Try
     End Sub
     Sub listen()
-        Console.ForegroundColor = ConsoleColor.White
-        Console.WriteLine("Listening for connections...")
-        While True
-            Dim client As TcpClient = server.AcceptTcpClient
-            Dim thread As New Threading.Thread(AddressOf listen)
-            thread.Start()
-			'Sequenz um Ip-Adressen zu speichern
-            Dim ep As Net.IPEndPoint = CType(client.Client.RemoteEndPoint, Net.IPEndPoint)
-            Dim ad As Integer = 0
-            RohAdressen = Split(ep.ToString, ":", 2)
-            Adressen(AdressenZaehler) = RohAdressen(0)
-            AdressenZaehler = AdressenZaehler + 1
-			'Sequenz ende
-            Nicks(AdressenZaehler) = ""
-            Console.ForegroundColor = ConsoleColor.Green
-            Console.WriteLine("Client {0} verbunden." & vbCrLf, client.Client.RemoteEndPoint)
-            While client.Connected
-                Try
-                    Dim stream As NetworkStream = client.GetStream()
-                    Dim i As Int32
-                    i = stream.Read(bytes, 0, bytes.Length)
-                    nachricht = Encoding.Default.GetString(bytes, 0, i)
-                    Console.ForegroundColor = ConsoleColor.Cyan
-                    If Nicks(AdressenZaehler) = "" Then
-                        Console.WriteLine("{0} um " & TimeOfDay & " Uhr", client.Client.RemoteEndPoint)
-                    Else
-                        Console.WriteLine("{0} um " & TimeOfDay & " Uhr", Nicks(AdressenZaehler))
-                    End If
-                    Console.ForegroundColor = ConsoleColor.White
-                    Console.WriteLine(nachricht)
-                    If nachricht.Substring(0, 1) = "/" Then
-                        Dim functions(1024) As Byte
-                        If getFunction(nachricht) = "help" Then
-                            functions = Encoding.Default.GetBytes("/whoami - Your IP" + vbCrLf + "/gettime - Get date and time" + vbCrLf + "/setnick - Set your nickname")
-                            stream.Write(functions, 0, functions.Length)
-                        ElseIf getFunction(nachricht) = "file" Then
-                            functions = Encoding.Default.GetBytes("Spaeter Funktion zur Datenuebertragung")
-                            stream.Write(functions, 0, functions.Length)
-                        ElseIf getFunction(nachricht) = "whoami" Then
-                            functions = Encoding.Default.GetBytes("whoami - You are " + Adressen(AdressenZaehler - 1))
-                            stream.Write(functions, 0, functions.Length)
-                        ElseIf getFunction(nachricht) = "gettime" Then
-                            functions = Encoding.Default.GetBytes("gettime - Today it's the " + Date.Now)
-                            stream.Write(functions, 0, functions.Length)
-                        ElseIf getFunction(nachricht) = "setnick" Then
-                            Try
-                                Nicks(AdressenZaehler) = nachricht.Substring(9, nachricht.Length - 9)
-                                If Nicks(AdressenZaehler) <> "" Then
-                                    functions = Encoding.Default.GetBytes("setnick - Your nickname is now " + nachricht.Substring(9, nachricht.Length - 9))
-                                Else
-                                    functions = Encoding.Default.GetBytes("Please enter a nickname after /setnick!")
-                                End If
-                            Catch ex As Exception
+        Dim client As TcpClient = server.AcceptTcpClient
+        Dim listenThread As New Threading.Thread(AddressOf listen)
+        listenThread.Start()
+        'Sequenz um Ip-Adressen zu speichern
+        Dim EndPoint As Net.IPEndPoint = CType(client.Client.RemoteEndPoint, Net.IPEndPoint)
+        Adressen(AdressenZaehler) = EndPoint
+        RohAdressen = Split(EndPoint.ToString, ":", 2)
+        ipAdressen(AdressenZaehler) = IPAddress.Parse(RohAdressen(0))
+        AdressenZaehler = AdressenZaehler + 1
+        'Sequenz ende
+        Nicks(AdressenZaehler) = ""
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("Client {0} verbunden." & vbCrLf, client.Client.RemoteEndPoint)
+        While client.Connected
+            Try
+                Dim stream As NetworkStream = client.GetStream()
+                Dim i As Int32
+                i = stream.Read(bytes, 0, bytes.Length)
+                nachricht = Encoding.Default.GetString(bytes, 0, i)
+                Console.ForegroundColor = ConsoleColor.Cyan
+                If Nicks(AdressenZaehler) = "" Then
+                    Console.WriteLine("{0} um " & TimeOfDay & " Uhr", client.Client.RemoteEndPoint)
+                Else
+                    Console.WriteLine("{0} um " & TimeOfDay & " Uhr", Nicks(AdressenZaehler))
+                End If
+                Console.ForegroundColor = ConsoleColor.White
+                Console.WriteLine(nachricht)
+                If nachricht.Substring(0, 1) = "/" Then
+                    Dim functions(1024) As Byte
+                    If getFunction(nachricht) = "help" Then
+                        functions = Encoding.Default.GetBytes("/whoami - Your IP" + vbCrLf + "/gettime - Get date and time" + vbCrLf + "/setnick - Set your nickname")
+                        stream.Write(functions, 0, functions.Length)
+                    ElseIf getFunction(nachricht) = "file" Then
+                        functions = Encoding.Default.GetBytes("Spaeter Funktion zur Datenuebertragung")
+                        stream.Write(functions, 0, functions.Length)
+                    ElseIf getFunction(nachricht) = "whoami" Then
+                        functions = Encoding.Default.GetBytes("whoami - You are " + Adressen(AdressenZaehler - 1).ToString)
+                        stream.Write(functions, 0, functions.Length)
+                    ElseIf getFunction(nachricht) = "gettime" Then
+                        functions = Encoding.Default.GetBytes("gettime - Today it's the " + Date.Now)
+                        stream.Write(functions, 0, functions.Length)
+                    ElseIf getFunction(nachricht) = "setnick" Then
+                        Try
+                            Nicks(AdressenZaehler) = nachricht.Substring(9, nachricht.Length - 9)
+                            If Nicks(AdressenZaehler) <> "" Then
+                                functions = Encoding.Default.GetBytes("setnick - Your nickname is now " + nachricht.Substring(9, nachricht.Length - 9))
+                            Else
                                 functions = Encoding.Default.GetBytes("Please enter a nickname after /setnick!")
-                            End Try
-                            stream.Write(functions, 0, functions.Length)
-                        Else
-                            functions = Encoding.Default.GetBytes("This is not a valid command. See /help for further information.")
-                            stream.Write(functions, 0, functions.Length)
-                        End If
+                            End If
+                        Catch ex As Exception
+                            functions = Encoding.Default.GetBytes("Please enter a nickname after /setnick!")
+                        End Try
+                        stream.Write(functions, 0, functions.Length)
+                    Else
+                        functions = Encoding.Default.GetBytes("This is not a valid command. See /help for further information.")
+                        stream.Write(functions, 0, functions.Length)
                     End If
-                Catch e As Exception
-                    Console.ForegroundColor = ConsoleColor.Red
-                    Console.WriteLine(vbCrLf & "Connection closed!")
-                    listen()
-                End Try
-            End While
+                End If
+            Catch e As Exception
+                Console.ForegroundColor = ConsoleColor.Red
+                Console.WriteLine(vbCrLf & "{0} closed the connection.", Nicks(AdressenZaehler))
+            End Try
         End While
     End Sub
     Function getFunction(ByVal nachricht) As String
